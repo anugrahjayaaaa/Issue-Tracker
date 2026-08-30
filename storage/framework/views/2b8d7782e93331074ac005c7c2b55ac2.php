3a5e09c36bd81143fe01f498950b8bc85711510a
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-08-30 22:45:05 +0700

    chore: untrack compiled Blade cache (storage/framework/views) (#8)
    Accidentally committed in 6f35f7d (P1.5). These are build artifacts
    regenerated on every view render, not source. Add to .gitignore and
    git rm --cached so they no longer pollute the tree.

--- storage/framework/views/2b8d7782e93331074ac005c7c2b55ac2.php
+++
@@ -1,4 +0,0 @@
-<footer class="app-footer">
-    <div class="ms-auto">Laravel Base Project</div>
-</footer>
-<?php /**PATH /Users/anugrahjayasakti/Projects/laravel/issue-tracker/resources/views/partials/layout/footer.blade.php ENDPATH**/ ?>
